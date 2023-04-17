28a9b7cd9d8908daad3ede71cc8b50010b947ff1
Thai Pham <phamquangthai97@gmail.com>
2023-04-17 11:55:37 +0700

    Handle POST request OK, add register function API

--- src/HttpMessage.h
+++ src/HttpMessage.h
@@ -91,34 +91,53 @@ namespace HttpMessage {
     HTTPVersion _version;
     HTTPMethod _method;
     HeadersMap _headers;
-    std::string _uri;
+    HeadersMap _queryParams;
+    std::string _path;
+    std::stringstream _body; //may have RAM issues with large request body
     std::istream& _request;
 
     HTTPRequest(std::istream& request) : _version(HTTPVersion::HTTP_1_1),
                                          _method(HTTPMethod::GET),
                                          _headers(),
-                                         _uri(),
+                                         _queryParams(),
+                                         _path(),
+                                         _body(),
                                          _request(request) {
-      _request >> *this;
+      // _request >> *this;
+      parse_request(_request);
     }
 
-    friend std::istream& operator>>(std::istream& is, HTTPRequest& obj) {
-      // read obj from stream
+    void parse_query_params(const std::string& path) {
+      auto startPos = path.find("?");
+      if (startPos == std::string::npos)
+        return;
+      auto queries = path.substr(startPos+1);
+      auto pairs = split_str(queries, "&");
+      for (const auto& pair : pairs) {
+        auto delimPos = pair.find("=");
+        if (delimPos != std::string::npos)
+          _queryParams[pair.substr(0, delimPos)] = pair.substr(delimPos+1);
+      }
+    }
+
+    // friend std::istream& operator>>(std::istream& is, HTTPRequest& obj) {
+    void parse_request(std::istream& is) {
       if(is.bad())
-        return is;
+        return;
 
       std::string line;
       std::string firstLine;
-      // first line is [method URI version]
+      // first line is [method path version]
       if(std::getline(is, firstLine, '\n')) {
         auto tokens = split_str(firstLine, " ");
         if(tokens.size() != 3) {
           std::cerr << "Error, invalid http message\n";
           throw std::runtime_error("invalid http message");
         }
-        obj._method = str_to_method(tokens[0]);
-        obj._uri = tokens[1];
-        obj._version = str_to_http_version(tokens[2]);
+        _method = str_to_method(tokens[0]);
+        _path = tokens[1];
+        parse_query_params(_path);
+        _version = str_to_http_version(trim_str(tokens[2]));
       }
       // next following lines are headers
       while(std::getline(is, line, '\n')) {
@@ -128,11 +147,28 @@ namespace HttpMessage {
         auto delimPos = trimLine.find(':');
         auto header = trimLine.substr(0, delimPos);
         auto value = trim_str(trimLine.substr(delimPos + 1));
-        obj._headers[header] = value;
+        _headers[header] = value;
       }
 
-      // remaining line is body
-      return is;
+      // remaining lines is body
+      while(std::getline(is, line)){
+        _body << line;
+      }
+      // return is;
+    }
+
+    std::string to_string() { // for debug only
+      std::cout << "version: " << version_str(_version) << ", method: " << method_str(_method) << "\n";
+      std::cout << "path: " << _path << "\n";
+      for (const auto& [key, value] : _headers) {
+        std::cout << key << ": " << value << '\n';
+      }
+      for (const auto& [key, value] : _queryParams) {
+        std::cout << key << ": " << value << '\n';
+      }
+      std::string body = _body.str();
+      std::cout << body << std::endl;
+      return "";
     }
   };
 
--- test.cpp
+++ test.cpp
@@ -16,34 +16,52 @@
 #include <unordered_map>
 
 #include <chrono>
+#include <filesystem>
 
 #include "src/HttpMessage.h"
-
-#define BUFFER_SIZE 4096
-#define QUEUEBACKLOG 100
-
-using namespace std::chrono;
-using namespace std::chrono_literals;
-
-using URLFormat = std::regex;
-using HandlerFunction = std::function<void(int)>;
-using HeadersMap = std::map<std::string, std::string>;
+#include "src/SimpleServer.h"
 
 using namespace HttpMessage;
+using namespace std::placeholders;
+namespace fs = std::filesystem;
 
-void* get_ip_address(struct sockaddr* sa) {
-  if(sa->sa_family == AF_INET) {
-    return &(((struct sockaddr_in*)sa)->sin_addr);
+void SendStaticFile(std::string path, int clientfd) {
+  static std::map<std::string, std::string> mimeTypes = {
+      {".txt", "text/plain"},
+      {".html", "text/html"},
+      {".htm", "text/html"},
+      {".css", "text/css"}};
+  if(!fs::exists(path)) {
+    // send 404 not found
+    send(clientfd, std::string{}.data(), 0, 0);
+    close(clientfd);
   }
-  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  auto extension = fs::path(path).extension().string();
+  std::stringstream ss;
+  // std::ifstream index("static/index.html");
+  std::ifstream index(path);
+  HTTPResponse response(index);
+  response._headers = {{{"Content-Length", std::to_string(content_length(index))},
+                        {"Connection", "keep-alive"}}};
+  if(mimeTypes.count(extension)) {
+    response._headers["Content-Type"] = mimeTypes.at(extension);
+  }
+  else {
+    response._headers["Content-Type"] = "application/octet-stream"; // default for others
+  }
+  ss << response;
+  auto sendText = ss.rdbuf()->str();
+  send(clientfd, sendText.data(), sendText.length(), 0);
+  close(clientfd);
 }
 
 int main(int argc, char* argv[]) {
-  std::ifstream ifs("../request-chrome.txt");
-  HTTPRequest request(ifs);
+  // std::ifstream ifs("../request-chrome.txt");
+  // HTTPRequest request(ifs);
   // std::cout << std::boolalpha << (HttpMessage::trim_str("      fsdflakjdfalkd   \r\n   \r\r   ") == std::string{"fsdflakjdfalkd"}) << std::endl;
   // std::cout << std::boolalpha << (HttpMessage::trim_str("\r\n").empty()) << std::endl;
-  // std::map<URLFormat, HandlerFunction> handlerMaps;
+  // std::cout << std::boolalpha << (HttpMessage::trim_str("123456789       \r\n") == std::string{"123456789"}) << std::endl;
+  // std::cout << std::boolalpha << (HttpMessage::trim_str("\r\n         123456789") == std::string{"123456789"}) << std::endl;
   if(argc != 3) {
     std::cerr << "Usage: [executable] <address> <port> \n";
     return -1;
@@ -51,67 +69,12 @@ int main(int argc, char* argv[]) {
   std::string address(argv[1]);
   auto port = stoi(std::string{argv[2]});
 
-  std::cout << "Simple server start listening on " << address << ":" << port << std::endl;
-  // int listenfd = 0, connfd = 0;
-  // auto timeNow = system_clock::now();
-  sockaddr_in serv_addr;
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  inet_pton(AF_INET, address.c_str(), &(serv_addr.sin_addr.s_addr));
-  serv_addr.sin_port = htons(port);
-
-  auto listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(listenfd < 0) {
-    throw std::runtime_error("Failed to open socket");
-    return -1;
-  }
-
-  if(bind(listenfd, (sockaddr*)&serv_addr, sizeof(serv_addr))) {
-    throw std::runtime_error("Failed to bind socket");
-    return -1;
-  }
-
-  if(listen(listenfd, QUEUEBACKLOG) < 0) {
-    throw std::runtime_error("Failed to listen socket");
-    return -1;
-  }
-
-  char buffer[BUFFER_SIZE];
-  memset(buffer, '0', sizeof(buffer));
-
-  while(1) {
-    sockaddr_storage connAddr;
-    char s[INET6_ADDRSTRLEN];
-    socklen_t connAddrSize = sizeof(connAddr);
-    auto fd = accept(listenfd, (struct sockaddr*)&connAddr, &connAddrSize);
-    if(fd == -1) {
-      // something went wrong
-      std::cerr << "Failed to accept incomming connection\n";
-      continue;
-    }
-
-    inet_ntop(connAddr.ss_family, get_ip_address((struct sockaddr*)&connAddr), s, sizeof(s));
-    std::cout << "Got connection from " << s << std::endl;
-
-    // std::string requestData;
-    // recv(fd, requestData.data(), sendText.length(), 0);
-    ssize_t byte_count = recv(fd, buffer, sizeof(buffer), 0);
-    if(byte_count > 0) {
-      std::cout << "Received request: \n" << std::string(buffer, byte_count);
-    }
-
-      std::stringstream ss;
-      std::ifstream index("index.html");
-      HTTPResponse response(index);
-      response._headers = {{{"Content-Type", "text/html"},
-                            {"Content-Length", std::to_string(content_length(index))},
-                            {"Connection", "keep-alive"}}};
-      ss << response;
-      auto sendText = ss.rdbuf()->str();
-      send(fd, sendText.data(), sendText.length(), 0);
-      close(fd);
-      std::this_thread::sleep_for(10ms);
-    }
-    return 0;
-  }
+  SimpleServer server(address, port);
+  server.AddHandlers({
+      {"/", {HTTPMethod::GET, std::bind(&SendStaticFile, "static/index.html", _1)}},
+      {"/styles.css", {HTTPMethod::GET, std::bind(&SendStaticFile, "static/styles.css", _1)}},
+  });
+  server.Start();
+  server.Listen();
+  return 0;
+}
