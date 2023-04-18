898b6ca2b1847a8e111573655d773c94b130f317
Thai Pham <phamquangthai97@gmail.com>
2023-04-18 14:44:22 +0700

    remove unused and debug files

--- remove-later/test-data.cpp
+++
@@ -1,6 +0,0 @@
-// std::ifstream ifs("../request-chrome.txt");
-// HTTPRequest request(ifs);
-// std::cout << std::boolalpha << (HttpMessage::trim_str("      fsdflakjdfalkd   \r\n   \r\r   ") == std::string{"fsdflakjdfalkd"}) << std::endl;
-// std::cout << std::boolalpha << (HttpMessage::trim_str("\r\n").empty()) << std::endl;
-// std::cout << std::boolalpha << (HttpMessage::trim_str("123456789       \r\n") == std::string{"123456789"}) << std::endl;
-// std::cout << std::boolalpha << (HttpMessage::trim_str("\r\n         123456789") == std::string{"123456789"}) << std::endl;
