672ab1066f026e82d671dcfa625ec705f64a9586
Thai Pham <thai.pham@opswat.com>
2023-08-16 14:56:15 +0700

    add dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -v $(readlink -e '.'):/opt/http-server opswat-ubuntu:20.04 /opt/http-server/build.sh --source_dir="/opt/http-server"
+docker run -v $(readlink -e '.'):/opt/http-server custom-ubuntu:20.04 /opt/http-server/build.sh --source_dir="/opt/http-server"
