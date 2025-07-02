9003359da35bd17129c5d22f3070e20b2b0868e6
thaipq <phamquangthai97@gmail.com>
2025-07-02 22:29:07 +0700

    update cmake build option with fsanitize address

--- sample_codes/BinarySearch.cpp
+++ sample_codes/BinarySearch.cpp
@@ -1,19 +1,39 @@
-int lower_bound(int* a, int64_t start, int64_t end, int value) {
-    // return index of first element >= value
-    auto low = start;
-    auto high = end;
-    for (low < high) {
-        mid = (low + high)/2;
-        if (a[mid] < value) { // change to <= for upper bound
-            low = mid + 1;
-        } else { // a[mid] >= value
-            high = mid;
+#include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+
+// #include "libs/RandomUtils.h"
+
+int lower_bound(const std::vector<int> &a, int target) {
+    if (a.empty()) return 0;
+    auto l = a[0];
+    auto h = a.size();
+    int m;
+    while (l < h) {
+        m = (l + h) / 2;
+        if (a[m] < target) {
+            l = m + 1;
+        } else {
+            h = m;
         }
     }
-    return low;
+    return l;
 }
 
-int binary_search(int* a, int64_t start, int64_t end, int value) {
-    auto i = lower_bound(a, start, end, value);
-    return i < end && a[i] == value;
+bool binary_search(const std::vector<int> &a, int target) {
+    auto i = lower_bound(a, target);
+    return (i < a.size() && a[i] == target);
 }
+
+// TEST(binary_search, tc1) {
+//     std::vector<int> a;
+//     for (auto i = 0; i < 100; i++) {
+//         a.push_back(libs::random_num(0, 1000));
+//     }
+//     std::sort(a.begin(), a.end());
+//     for (auto i = 0; i < 50; i++) {
+//         auto v = libs::random_num(0, 1000);
+//         auto d = std::lower_bound(a.begin(), a.end(), v) - a.begin();
+//         ASSERT_EQ(d, lower_bound(a, v));
+//     }
+// }
