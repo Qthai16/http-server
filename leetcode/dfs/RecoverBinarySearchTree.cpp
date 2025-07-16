92944db384ef19c033da72066386577e4d59f35c
thaipq <phamquangthai97@gmail.com>
2025-07-16 23:06:21 +0700

    add leetcode solutions

--- leetcode/dfs/RecoverBinarySearchTree.cpp
+++ leetcode/dfs/RecoverBinarySearchTree.cpp
@@ -10,6 +10,15 @@
  * };
  */
 
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -19,19 +28,11 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-#include <vector>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <deque>
-#include <cstdio>
-using namespace std;
-
 // for every sub-tree: left < root < right
 class Solution {
-    void dfs(TreeNode *node, stack<TreeNode*>& s, vector<pair<int, TreeNode*>>& arr) {
+    void dfs(TreeNode *node, stack<TreeNode *> &s, vector<pair<int, TreeNode *>> &arr) {
         if (node == nullptr) {
-            if(!s.empty()) s.pop();
+            if (!s.empty()) s.pop();
             return;
         }
         if (node->left == nullptr && node->right == nullptr) {
@@ -50,14 +51,56 @@ class Solution {
         }
         if (!s.empty()) s.pop();
     }
-    void swapNode(TreeNode* l, TreeNode* r) {
+
+    void morrisTraversal(TreeNode *node, pair<TreeNode *, TreeNode *>& swapPair) {
+        // traversal + compare prev->val vs cur->val
+        TreeNode *prev{nullptr}, *curr{nullptr}, *node1{nullptr}, *node2{nullptr};
+        curr = node;
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                // printf("(%d)", curr->val);
+                node1 = node2;
+                node2 = curr;
+                curr = curr->right;
+            } else {
+                prev = curr->left;
+                while (prev->right != nullptr && prev->right != curr)
+                    prev = prev->right;
+
+                // Make curr as the right child of its previous
+                if (prev->right == nullptr) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {// fix the right child of previous
+                    prev->right = nullptr;
+                    // printf("[%d]", curr->val);
+                    node1 = node2;
+                    node2 = curr;
+                    curr = curr->right;
+                }
+            }
+            if (node1 && node2 && node1->val > node2->val) {
+                // printf("got pair (%d): (%d)\n", node1->val, node2->val);
+                if (!swapPair.first && !swapPair.second) {
+                    swapPair.first = node1;
+                    swapPair.second = node2;
+                } else {
+                    swapPair.second = node2;
+                }
+            }
+        }
+    }
+
+    void swapNode(TreeNode *l, TreeNode *r) {
         swap(l->val, r->val);
     }
+
 public:
-    void recoverTree(TreeNode* root) {
+    void recoverTree(TreeNode *root) {
+        // solution 1: use stack + recursion in order traversal, then check sorted array
         if (root == nullptr) return;
-        stack<TreeNode*> s;
-        vector<pair<int, TreeNode*>> arr;
+        stack<TreeNode *> s;
+        vector<pair<int, TreeNode *>> arr;
         s.push(root);
         while (!s.empty()) {
             dfs(s.top(), s, arr);
@@ -65,12 +108,23 @@ public:
         vector<int> ind;
         for (auto i = 0; i < arr.size(); i++) {
             if (i + 1 >= arr.size()) break;
-            if (arr[i].first < arr[i+1].first) continue;
+            if (arr[i].first < arr[i + 1].first) continue;
             ind.push_back(i);
         }
         if (ind.size() == 1)
-            swapNode(arr[ind[0]].second, arr[ind[0]+1].second);
+            swapNode(arr[ind[0]].second, arr[ind[0] + 1].second);
         else if (ind.size() == 2)
-            swapNode(arr[ind[0]].second, arr[ind[1]+1].second);
+            swapNode(arr[ind[0]].second, arr[ind[1] + 1].second);
+    }
+
+    void recoverTree(TreeNode *root) {
+    // solution 2: use morris traversal + compare last node with current node
+        if (root == nullptr) return;
+        pair<TreeNode *, TreeNode *> swapNodes{nullptr, nullptr};
+        morrisTraversal(root, swapNodes);
+        // fflush(stdout);
+        if (swapNodes.first && swapNodes.second) {
+            swapNode(swapNodes.first, swapNodes.second);
+        }
     }
 };
